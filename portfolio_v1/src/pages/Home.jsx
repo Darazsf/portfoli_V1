16d71a2e6b4eccd9c104ce2a0dba035be5bc30a5
Darazsf <darazs_ferenc@hotmail.com>
2023-09-21 14:11:20 +0200

    homepage

--- portfolio_v1/src/pages/Home.jsx
+++ portfolio_v1/src/pages/Home.jsx
@@ -1,15 +1,18 @@
 import "../css/home.css"
 
-      
-export default function Home(){
 
-    return(
+export default function Home() {
+
+    return (
         <>
-        
-        <div className="home-contanier">
-              <div>picture</div>  
-              <div>pár szó rolam</div>  
-        </div>
+    <div className="home-container">
+            <div className="me">
+                <div><img className="sepia" src="/DSCF0352-Edit.jpg" alt="me" width="300" /></div>
+                <div>Hello my name is Feri
+                    <p>Thank you for visiting my first website! Here you'll find some information about me,
+                    I'll upload and keep updated all my relevant works in my Portfolio</p></div>
+            </div>
+            </div>
         </>
     )
 }
