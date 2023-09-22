af3a091b89ae45a47c4d49eb6e270b39c96aa003
Darazsf <darazs_ferenc@hotmail.com>
2023-09-22 16:34:56 +0200

    about

--- portfolio_v1/src/pages/About.jsx
+++ portfolio_v1/src/pages/About.jsx
@@ -2,6 +2,36 @@
 
 export default function About(){
     return(
-        <h1>About</h1>
+        <>
+            <div className="">
+                <h1>Hello</h1>
+                <h2>I'm Feri,</h2>
+                <p>After 10+ years in the aviation world, I was  looking for new chellenges</p>
+                <p>So in October 2022, I started to study programming after I had been around the subject
+                    and really liked in. In June 2023,
+                    I successfully passes the Progmatic Academy exam</p>
+                    <h2>Current Goals</h2>
+                    <ul>
+                        <li>Learn more with a great team</li>
+                        <li>Learn more with a great team</li>
+                        <li>Learn more with a great team</li>
+                        <li>Learn more with a great team</li>
+                    </ul>
+            </div>
+
+
+            <div className="skills-container">
+                <h2>Programming skills</h2>
+                <div>
+                    <ul>
+                        <li>React</li>
+                        <li>Javascript</li>
+                        <li>Css</li>
+                        <li>Html5</li>
+                    </ul>
+                </div>
+            </div>
+
+        </>
     )
 }
--- portfolio_v1/src/pages/Contact.jsx
+++ portfolio_v1/src/pages/Contact.jsx
@@ -1,5 +1,17 @@
 export default function Contact(){
     return(
-        <h1>Contact</h1>
+        <>
+             <div>
+                <ul>
+                    <li>18.10.1987</li>
+                    <li>Budapest, Hungary</li>
+                    <li>+36 30-297-22-58</li>
+                    <li>darazs.feri@gmail.com</li>
+                    <li>Linkedin</li>
+                    <li>Github</li>
+                </ul>
+            </div>
+              
+        </>
     )
 }
