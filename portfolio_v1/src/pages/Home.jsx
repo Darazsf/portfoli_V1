136b2303c81074fa7344c62222fd0760470d70da
Darazsf <darazs_ferenc@hotmail.com>
2023-09-25 18:41:32 +0200

    upgrade

--- portfolio_v1/src/pages/Home.jsx
+++ portfolio_v1/src/pages/Home.jsx
@@ -8,9 +8,12 @@ export default function Home() {
     <div className="home-container">
             <div className="me">
                 <div><img className="sepia" src="/DSCF0352-Edit.jpg" alt="me" width="300" /></div>
-                <div className="welcome">Hello my name is Feri
-                    <p>Thank you for visiting my first website! Here you'll find some information about me,
-                    I'll upload and keep updated all my relevant works in my Portfolio</p></div>
+                <div className="welcome"><h3>Hello and welcome to my portfolio page!</h3>
+                    <p>My name is Feri and I'm excited to have you here on my first website.
+                        Take a look around to get to know me better, and stay tuned for updates as I showcase
+                        my work in this creative space.
+                        </p>
+                        </div>
             </div>
             </div>
         </>
