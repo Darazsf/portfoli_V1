cbb6fac7694597fd1ff0aa9812e662aebeaf2c84
Darazsf <darazs_ferenc@hotmail.com>
2023-09-26 16:13:48 +0200

    contact-page

--- portfolio_v1/src/pages/Contact.jsx
+++ portfolio_v1/src/pages/Contact.jsx
@@ -2,36 +2,46 @@
 import "../css/contact.css"
 
 
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <>
             <div className="contact-container">
-                <h2>Contact</h2>
+                <div><h2>Contact</h2></div>
                 <div className="contact">
-                    <ul>
-                        <li> <span className="material-symbols-outlined">
-                            calendar_month
-                        </span>18.10.1987</li>
+                    <ul className="con-icon">
+
                         <li> <span className="material-symbols-outlined">
                             home_pin
-                        </span>Budapest, Hungary</li>
+                        </span>
+                        <p>Budapest, Hungary</p>
+                        </li>
+                        
 
                         <li><span className="material-symbols-outlined">
                             call
-                        </span><span>+36 30-297-22-58</span></li>
+                        </span>
+                        <p>+36 30-297-22-58</p>
+                        </li>
+                       
 
-                        <li> <span className="material-symbols-outlined">
-                            mail
-                        </span><a href="mailto: darazs.feri@gmail.com">Send Email</a></li>
-
+                        <li>
+                            <span class="material-symbols-outlined">
+                                mail
+                            </span>
+                            <p>darazs.feri@gmail.com</p>
+                        </li>
                         
+
+
                     </ul>
                 </div>
                 <div>
-
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21589.51995483613!2d19.231111249999998!3d47.437487899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1shu!2shu!4v1695737372836!5m2!1shu!2shu" width="600" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
+                
             </div>
-              
+            
+
         </>
     )
 }
