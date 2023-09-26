ede770562f16c6627bfe0551a3c0a9927b2e77d2
Darazsf <darazs_ferenc@hotmail.com>
2023-09-26 16:55:58 +0200

    nav-up

--- portfolio_v1/src/components/Navbar.js
+++ portfolio_v1/src/components/Navbar.js
@@ -16,55 +16,55 @@ const Navbar = () => {
     
       <nav className="navbar">
         <div className="container">
-        <div className="brand">
-          <h1><a href="/" className="name">Darázs Ferenc</a></h1>
-        </div>
-        <div className="nav-elements">
-          <ul>
-            <li><NavLink
-              to="/"
-              className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "active" : ""
-              }
-            >
-              <span> {"< "}Home{"/>"}</span>
-            </NavLink>
-            </li>
+         
+            <h1><a href="/" className="name">Darázs Ferenc</a></h1>
+       
+          <div className="nav-elements">
+            <ul>
+              <li><NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <span> {"< "}Home{"/>"}</span>
+              </NavLink>
+              </li>
 
-            <li> <NavLink
-              to="/about"
-              className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "active" : ""
-              }
-            >
-              <span> {"< "}About{"/>"}</span>
-            </NavLink></li>
-            <li> <NavLink
-              to="/projects"
-              className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "active" : ""
-              }
-            >
-              <span> {"< "}Projects{"/>"}</span>
-            </NavLink></li>
-            <li>  <NavLink
-              to="/contact"
-              className={({ isActive, isPending }) =>
-                isPending ? "pending" : isActive ? "active" : ""
-              }
-            >
-              <span> {"< "}Contact{"/>"}</span>
-            </NavLink></li>
-          </ul>
+              <li> <NavLink
+                to="/about"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <span> {"< "}About{"/>"}</span>
+              </NavLink></li>
+              <li> <NavLink
+                to="/projects"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <span> {"< "}Projects{"/>"}</span>
+              </NavLink></li>
+              <li>  <NavLink
+                to="/contact"
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? "active" : ""
+                }
+              >
+                <span> {"< "}Contact{"/>"}</span>
+              </NavLink></li>
+            </ul>
 
-          {/* Dropdown menu */}
-        </div>
+            {/* Dropdown menu */}
+          </div>
           <div className="toggle-btn"><i onClick={openMenu()}><span class="material-symbols-outlined">
             menu
           </span></i></div>
 
           <div className="dropdown-menu">
-          <li><NavLink
+            <li><NavLink
               to="/"
               className={({ isActive, isPending }) =>
                 isPending ? "pending" : isActive ? "active" : ""
