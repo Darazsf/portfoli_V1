956a73c5157c7519edc366ed71e7ef47e1a982e5
Darazsf <darazs_ferenc@hotmail.com>
2023-10-13 11:44:22 +0200

    up

--- portfolio_v1/src/components/Layout.js
+++ portfolio_v1/src/components/Layout.js
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { useState } from "react";
-import { ThemeContext } from '../context/ThemeContext';
 import "../context/ThemeContext"
 import "../css/theme.css"
 
