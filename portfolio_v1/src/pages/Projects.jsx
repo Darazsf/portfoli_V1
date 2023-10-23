9b6cea57042f28817305c6f9854ef36db6fec729
Darazsf <darazs_ferenc@hotmail.com>
2023-10-23 22:55:32 +0200

    projects-upload

--- portfolio_v1/src/pages/Projects.jsx
+++ portfolio_v1/src/pages/Projects.jsx
@@ -1,9 +1,80 @@
-export default function Projects(){
+import webshop from "../assets/webshop.png"
+import under from "../assets/under.jpg"
+import "../css/projects.css"
 
-    return(
+
+export default function Projects() {
+
+    return (
         <>
-        <h1>Projects</h1>
-        <p>Coming soon....</p>
+            <div className="project-wrapper">
+
+
+                <div className="wrapper">
+                    <div className="card">
+                        <div className="face face1">
+                            <img className="card-img" src={webshop} alt="webshop" />
+                        </div>
+                        <div className="face face2">
+                            <div className="content">
+                                <h3>
+                                    <a href="https://team8-webshop.vercel.app/" target="_blank">Webshop</a>
+                                </h3>
+                                <p>My classNamemates and I collaborated on this project, creating an online toy store as our final assignment for the bootcamp.</p>
+                                <div className="button-wrapper">
+                                <a href="https://github.com/Darazsf/webshop" target="_blank"><button className="btn outline">Source code</button></a>
+                                    <a href="https://team8-webshop.vercel.app/" target="_blank"> <button className="btn fill">Live</button></a>
+                                   
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="wrapper">
+                    <div className="card">
+                        <div className="face face1">
+                            <img className="card-img" src={under} alt="webshop" />
+                        </div>
+                        <div className="face face2">
+                            <div className="content">
+                                <h3>
+                                    <a href="" target="_blank">Under construction</a>
+                                </h3>
+                                <p></p>
+                                
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+
+                <div className="wrapper">
+                    <div className="card">
+                        <div className="face face1">
+                            <img className="card-img" src={under} alt="webshop" />
+                        </div>
+                        <div className="face face2">
+                            <div className="content">
+                                <h3>
+                                    <a href="" target="_blank">Under construction</a>
+                                </h3>
+                                <p></p>
+                                
+                                
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+               
+
+
+
+            </div>
+
         </>
     )
 }
