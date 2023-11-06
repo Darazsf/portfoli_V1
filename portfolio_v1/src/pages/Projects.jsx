81506ccd73b64ff7532e5e2d1d82545cbd00a924
Darazsf <darazs_ferenc@hotmail.com>
2023-11-06 15:34:32 +0100

    next-project

--- portfolio_v1/src/pages/Projects.jsx
+++ portfolio_v1/src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import webshop from "../assets/webshop.png"
 import under from "../assets/under.jpg"
+import load from "../assets/loadsheet.png"
 import "../css/projects.css"
 
 
@@ -16,10 +17,9 @@ export default function Projects() {
                             <img className="card-img" src={webshop} alt="webshop" />
                         </div>
                         <div className="face face2">
+                            
                             <div className="content">
-                                <h3>
-                                    <a href="https://team8-webshop.vercel.app/" target="_blank">Webshop</a>
-                                </h3>
+                                
                                 <p>My classmates and I collaborated on this project, creating an online toy store as our final assignment for the bootcamp.</p>
                                 <div className="button-wrapper">
                                 <a href="https://github.com/Darazsf/webshop" target="_blank"><button className="btn outline">Source code</button></a>
@@ -36,14 +36,19 @@ export default function Projects() {
                 <div className="wrapper">
                     <div className="card">
                         <div className="face face1">
-                            <img className="card-img" src={under} alt="webshop" />
+                            <img className="card-img" src={load} alt="loadsheet" />
                         </div>
                         <div className="face face2">
                             <div className="content">
-                                <h3>
-                                    <a href="" target="_blank">Under construction</a>
-                                </h3>
-                                <p></p>
+                                
+                                    <p>A defining part of my life so far has been calculating aircraft takeoff/landing weights on paper. I have now implemented a portion of it using simple JavaScript with DOM manipulation to achieve the end result. Minimal CSS</p>
+                                    <div className="button-wrapper">
+                                <a href="https://github.com/Darazsf/Loadsheet" target="_blank"><button className="btn outline">Source code</button></a>
+                                    <a href="https://darazsf.github.io/Loadsheet/" target="_blank"> <button className="btn fill">Live</button></a>
+                                   
+                                </div>
+                                
+                            
                                 
                             </div>
                            
