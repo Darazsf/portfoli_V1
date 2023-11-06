9576f4a0bdfe686dc42a955ed8488f23d2161bea
Darazsf <darazs_ferenc@hotmail.com>
2023-11-06 16:45:25 +0100

    new-project-rock

--- portfolio_v1/src/pages/Projects.jsx
+++ portfolio_v1/src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import webshop from "../assets/webshop.png"
 import under from "../assets/under.jpg"
 import load from "../assets/loadsheet.png"
+import rock from "../assets/rock-paper.png"
 import "../css/projects.css"
 
 
@@ -55,7 +56,48 @@ export default function Projects() {
                         </div>
                     </div>
                 </div>
+                <div className="wrapper">
+                    <div className="card">
+                        <div className="face face1">
+                            <img className="card-img" src={rock} alt="loadsheet" />
+                        </div>
+                        <div className="face face2">
+                            <div className="content">
+                                
+                                    <p>A defining part of my life so far has been calculating aircraft takeoff/landing weights on paper. I have now implemented a portion of it using simple JavaScript with DOM manipulation to achieve the end result. Minimal CSS</p>
+                                    <div className="button-wrapper">
+                                <a href="https://github.com/Darazsf/Loadsheet" target="_blank"><button className="btn outline">Source code</button></a>
+                                    <a href="https://darazsf.github.io/Loadsheet/" target="_blank"> <button className="btn fill">Live</button></a>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+                <svg class="arrows">
+              <path class="a1" d="M0 0 L30 32 L60 0"></path>
+              <path class="a2" d="M0 20 L30 52 L60 20"></path>
+              <path class="a3" d="M0 40 L30 72 L60 40"></path>
+            </svg>
 
+                <div className="wrapper">
+                    <div className="card">
+                        <div className="face face1">
+                            <img className="card-img" src={under} alt="webshop" />
+                        </div>
+                        <div className="face face2">
+                            <div className="content">
+                                <h3>
+                                    <a href="" target="_blank">Under construction</a>
+                                </h3>
+                                <p></p>
+                                
+                                
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
                 <div className="wrapper">
                     <div className="card">
                         <div className="face face1">
